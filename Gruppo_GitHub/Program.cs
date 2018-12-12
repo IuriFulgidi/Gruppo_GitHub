51e69a8d853be98e9f71d3e8e03e5f3015da0a3e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:40:49 +0100

    progetto iniziale

--- Gruppo_GitHub/Program.cs
+++ Gruppo_GitHub/Program.cs
@@ -13,6 +13,14 @@ namespace Gruppo_GitHub
             string risultato;
             int num = 255;
             risultato = "";
+            while(num>1)
+            {
+                int resto = num % 2;
+                risultato = Convert.ToString(resto) + risultato;
+                num = num / 2;
+            }
+            risultato = Convert.ToString(num) + risultato;
+            Console.WriteLine($"il numero binario è {risultato}");
         }
     }
 }
