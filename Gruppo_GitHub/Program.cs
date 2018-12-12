4719274d0fae22cbe0e4e26d5d155289e3650135
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:50:26 +0100

    l' utnte può inserire il numero da convertire

--- Gruppo_GitHub/Program.cs
+++ Gruppo_GitHub/Program.cs
@@ -10,14 +10,31 @@ namespace Gruppo_GitHub
     {
         static void Main(string[] args)
         {
+            //inizializzano le variabili
             string risultato="";
             int num = 255;
+
+            //si chiede all' utente il numero da convertire
+            do
+            {
+                Console.WriteLine("inserire il numero da convertire");
+                num = Convert.ToInt32(Console.ReadLine());
+                if(num<0)
+                {
+                    Console.WriteLine("inserire un numero positivo");
+                }
+            }
+            while (num < 0);
+
+            //si converte il numero
             while(num>1)
             {
                 int resto = num % 2;
                 risultato = Convert.ToString(resto) + risultato;
                 num = num / 2;
             }
+
+            //si stampa il risultato
             risultato = Convert.ToString(num) + risultato;
             Console.WriteLine($"il numero binario è {risultato}");
         }
