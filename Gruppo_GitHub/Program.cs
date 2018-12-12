0f8aa3b30616fcf5e4b66374493f45031eb82718
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:27:24 +0100

    Add project files.

---
+++ Gruppo_GitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppo_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string risultato;
+        }
+    }
+}
